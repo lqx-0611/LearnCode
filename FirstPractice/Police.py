a0267eebca63da575e2d4d7e6b21014f5232bf14
lqx-0611 <18821708052@163.com>
2021-04-06 20:30:41 +0800

    哈哈哈哈

---
+++ FirstPractice/Police.py
@@ -0,0 +1,14 @@
+
+from Hero import hero
+from Timo import timo
+
+class police(hero):
+    hp = 9000
+    power = 200
+    Hero_name = 'Police'
+
+# Police = police()
+# Timo = timo()
+# Police.fight(Timo.hp,Timo.power)
+# # Timo.fight(Police.hp,Police.power)
+# Police.speak_lines()
